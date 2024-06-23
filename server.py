9b793faea1468264a5b81174bd2aaaf19948d7be
Jhay <jeremiechiclote34@gmail.com>
2024-06-23 18:52:36 +0200

    Llamada api externa

--- server.py
+++ server.py
@@ -1,19 +1,39 @@
 
-from flask import Flask
+from flask import Flask, request, jsonify
+import requests 
 
 app = Flask(__name__)
 
 # Members API Route
 
-@app.route("/members")
+@app.route("/buscarJugador", methods=['GET'])
+def buscar_jugador():
+    try:
+        player_name = request.args.get('player_name')
+        if not player_name:
+            return jsonify({'error': 'Player name parameter is required'}), 400
 
+        api_url = f'https://transfermarkt-api.fly.dev/players/search/{player_name}'
+        response = requests.get(api_url)
+
+        if response.status_code == 200:
+            results = response.json()
+        else:
+            results = {'error': 'Unable to fetch data from API'}
+
+        return jsonify(results)
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+@app.route("/members")
 def members():
     return {"members": ["Member1", "Member2"]}
 
 @app.route("/cliente")
 
 def cliente():
-    print("hola mundo");
     return {"members": ["Arturo", "Se come a la prima del Jey"]}
 
 if __name__ == "__main__":
